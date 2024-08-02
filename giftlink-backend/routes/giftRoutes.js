60caebec3fdd43ac3a6fcb380ab900cf02e07ae5
romainmaubry <80488466+romainmaubry@users.noreply.github.com>
2024-08-02 09:22:25 -0400

    Update giftRoutes.js

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -6,13 +6,13 @@ const express = require('express');
 router.get('/', async (req, res) => {
     try {
         // Task 1: Connect to MongoDB and store connection to db constant
-        const db = await connectToDatabase()
+        const db = await connectToDatabase();
 
         // Task 2: use the collection() method to retrieve the gift collection
-       const collection = db.collection("gifts")
+       const collection = db.collection("gifts");
 
         // Task 3: Fetch all gifts using the collection.find method. Chain with toArray method to convert to JSON array
-        const gifts = await collection.find({}).toArray()
+        const gifts = await collection.find({}).toArray();
 
         // Task 4: return the gifts using the res.json method
         res.json(gifts);
